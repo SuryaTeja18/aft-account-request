7a209726f7ca36711f91225f4fc5e54e807d5dc9
SuryaTeja18 <18suryateja@gmail.com>
2025-07-07 14:33:50 +0530

    Create account-requests.tf

---
+++ terraform/account-requests.tf
@@ -0,0 +1,29 @@
+module "account_request_01" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "sakalak+aft001@amazon.com"
+    AccountName               = "AFT01"
+    ManagedOrganizationalUnit = "Sandbox (ou-jl8v-kolgi4le)" 
+    SSOUserEmail              = "sakalak+aft001@amazon.com"
+    SSOUserFirstName          = "AFT"
+    SSOUserLastName           = "001"
+  }
+
+  account_tags = {
+    "ABC:BUCode"      = "ABC003"
+    "ABC:Project"     = "123456"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "AWS Control Tower Lab"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform (AFT)"
+  }
+
+  custom_fields = {
+    custom1 = "a"
+    custom2 = "b"
+  }
+
+  account_customizations_name = "SANDBOX"
+}
